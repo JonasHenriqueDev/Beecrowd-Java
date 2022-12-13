93035ebdb2ea3db8d94c25f103ec06e5707a096f
Jonas <jonashenrique965@gmail.com>
2022-12-13 16:23:56 -0300

    Primeiro commit

---
+++ src/ExtremamenteBásico1001.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ExtremamenteBásico1001 {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        System.out.printf("X = %d%n", (A + B));
+    }
+
+}
---
+++ src/ÁreaDoCírculo1002.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ÁreaDoCírculo1002 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double n = 3.14159;
+        double raio = scan.nextDouble();
+        double area = n * (raio * raio);
+        System.out.printf("A=%.4f%n", area);
+    }
+}
